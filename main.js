81635eb6e59d63771a903f74c9ff3083dfd79a81
kurururu2004 <Kurmanbekovaarina20003@gmail.com>
2022-09-23 20:07:25 +0300

    dz4

---
+++ main.js
@@ -0,0 +1,41 @@
+// let arr = "*";
+// while (arr.length <= 7) {
+//   arr += "*";
+//     console.log(arr);
+// }
+
+
+// var slash = '#';
+// var space = ' ';
+// var result = '';
+// var row = true;
+// for (var i = 0; i < 8; i++) {
+//   for (var j = 0; j < 8; j++) {
+//     if ((j+row) / 2 === 0) {
+//       result += space;
+//     } else {
+//       result += slash;
+//     }
+//   }
+//   console.log(result);
+//   result='';
+//   row = ! row;
+// }
+
+
+
+
+// const numbers = [];
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//         numbers.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         numbers.push("Fizz");
+//     } else if (i % 5 === 0) {
+//         numbers.push("Buzz");
+//     } else {
+//         numbers.push(i);
+//     }
+// }
+// console.log(numbers);
